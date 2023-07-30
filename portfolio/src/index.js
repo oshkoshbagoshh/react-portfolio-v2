06665eecd5ae1f5befcec739018d314591ff64b5
AJ <amirjavadi25@gmail.com>
2023-07-30 15:57:58 -0500

    updated components

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+    {/*  */}
 
-<h1>Hello, world!</h1>,
-
+  </React.StrictMode>
 
 
 
